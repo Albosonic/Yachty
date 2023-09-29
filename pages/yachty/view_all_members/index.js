2caf661d19981734a4290d1a16ba573a3ca67aa2
albosonic <albertomadueno@gmail.com>
2023-09-29 11:21:52 -0700

    wip

---
+++ pages/yachty/view_all_members/index.js
@@ -0,0 +1,13 @@
+import AllMembersTable from "@/components/AllMembersComponent"
+
+
+const ViewAllMembers = () => {
+
+  return (
+    <>
+      <AllMembersTable />
+    </>
+  )
+};
+
+export default ViewAllMembers;
