527e26edace0d2ed0d0be4cc1e7e9e422a4de60a
albosonic <albertomadueno@gmail.com>
2023-11-07 07:55:05 -0800

    title and start direction

--- components/UploadRaceCourse.js
+++ components/UploadRaceCourse.js
@@ -1,16 +1,23 @@
 import { useState } from "react";
-import { Button, FormControl, FormControlLabel, Grid, IconButton, List, ListItemButton, ListItem, Paper, Radio, RadioGroup, Stack, TextField, Typography, Fab, useMediaQuery } from "@mui/material";
+import { Button, FormControl, FormControlLabel, Grid, IconButton, List, ListItemButton, ListItem, Paper, Radio, RadioGroup, Stack, TextField, Typography, Fab, useMediaQuery, FormLabel } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
 import ListItemText from '@mui/material/ListItemText';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import { useMutation } from "@apollo/client";
+import { INSERT_RACE_COURSE } from "@/lib/gqlQueries/racinggql";
+import { useSelector } from "react-redux";
 
 
 const UploadRaceCourse = () => {
   // const clearWorkingLeg = {marker: null, side: null}
+  const cleanCourseNameInfo = {name: '', set: false, startFrom: null};
   const [userInput, setUserInput] = useState('');
+  const [courseNameInfo, setCourseNameInfo] = useState({name: '', set: false});
   const [workingLeg, setWorkingLeg] = useState({marker: null, side: null});
   const [course, setCourse] = useState([]);
   const moreThan600px = useMediaQuery('(min-width:600px)');
+  const ycId = useSelector(state => state.auth.member.yachtClubByYachtClub.id);
+  const [insertRaceCourse, {loading: courseLoading}] = useMutation(INSERT_RACE_COURSE);
 
   const addCourseLeg = () => {
     setCourse([...course, workingLeg]);
@@ -25,34 +32,82 @@ const UploadRaceCourse = () => {
     }
   };
 
+  const uploadRaceCourse = async () => {
+    await insertRaceCourse({variables: { courseName: 'bales', ycId: ycId, instructions: course }})
+  }
+
   const { marker, side } = workingLeg;
+  const { name: courseTitle, set: titleSet, startFrom } = courseNameInfo;
   const containerWidth =  moreThan600px ? 700 : 350;
   return (
     <>
-      <Stack width="100%" maxWidth={containerWidth} spacing={3} alignContent="center">        
+      <Stack width="100%" maxWidth={containerWidth} spacing={3} alignContent="center">
         <Fab sx={{maxWidth: 150, alignSelf: 'flex-end'}} onClick={console.log('upload course')} color="success" variant="extended">
           <AddIcon />
           upload
         </Fab>
+        {!titleSet && <TextField
+          placeholder="Enter course name"
+          multiline
+          onChange={(e) => setCourseNameInfo({...courseNameInfo, name: e.target.value})}
+          value={courseTitle}
+          variant="standard"
+          sx={{width: '100%'}}
+          InputProps={{endAdornment: <Button onClick={() => setCourseNameInfo({...courseNameInfo, set: true})}>Set</Button>}}
+        />}
         <List>
-          {course.map((leg, index) => {
-            const {marker, side} = leg;            
-            return (              
-              <ListItem
-              key={marker + side + index}
+          {titleSet &&
+          <>          
+            <ListItem            
+              sx={{width: '100%', display: 'flex', justifyContent: 'center'}}
               secondaryAction={
-                <IconButton onClick={() => deleteCourseLeg(index)} edge="end" aria-label="comments">
+                <IconButton onClick={() => setCourseNameInfo({name: '', set: false})} edge="end" aria-label="course-title">
                   <DeleteForeverIcon/>
                 </IconButton>
               }
-              disablePadding
-            >
-              <ListItemText id={index} primary={`${ index + 1 }. ${ marker } To ${ side } `} />
-            </ListItem>              
+                disablePadding
+              >
+              <ListItemText primaryTypographyProps={{variant: 'h4'}} id={courseTitle} primary={courseTitle} />
+            </ListItem>
+            {startFrom && <Typography> start from: { startFrom}</Typography>}
+            <FormControl sx={{width: '100%'}}>
+              <FormLabel id="demo-form-control-label-placement">Choose Start Direction</FormLabel>
+              <RadioGroup
+                row
+                aria-labelledby="demo-row-radio-buttons-group-label"
+                name="row-radio-buttons-group"
+                sx={{justifyContent: 'space-around'}}
+                onChange={(e) => setCourseNameInfo({ ...courseNameInfo, startFrom: e.target.value })}
+              >
+                <FormControlLabel labelPlacement="start" value="West" control={<Radio />} label="West" />
+                <Stack>
+                  <FormControlLabel labelPlacement="top" value="North" control={<Radio />} label="North" />
+                  <FormControlLabel labelPlacement="bottom" value="South" control={<Radio />} label="South" />
+                </Stack>
+                <FormControlLabel value="East" control={<Radio />} label="East" />
+              </RadioGroup>
+            </FormControl>
+          </>}
+
+          {course.map((leg, index) => {
+            const {marker, side} = leg;
+            return (
+              <ListItem
+                key={marker + side + index}
+                secondaryAction={
+                  <IconButton onClick={() => deleteCourseLeg(index)} edge="end" aria-label="comments">
+                    <DeleteForeverIcon/>
+                  </IconButton>
+                }
+                  disablePadding
+                >
+                <ListItemText id={index} primary={`${ index + 1 }. ${ marker } To ${ side } `} />
+              </ListItem>
             )
           })}
         </List>
-        {(marker || side) && <Paper elevation={4} sx={{padding: 2}}>
+        {(marker || side) &&
+        <Paper elevation={4} sx={{padding: 2}}>
           <Grid container flexWrap="nowrap" justifyContent="space-around">
             {marker && <Typography>Marker: {marker}</Typography>}
             {side && <Typography> To: {side}</Typography>}
