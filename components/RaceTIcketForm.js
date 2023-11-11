898b29ae8a0e092a6c3e52d12d156e31a100a4c3
albosonic <albertomadueno@gmail.com>
2023-11-11 11:39:26 -0800

    move ticket input

--- components/RaceTIcketForm.js
+++ components/RaceTIcketForm.js
@@ -1,4 +1,4 @@
-import { Alert, Box, Card, CardContent, CardMedia, CircularProgress, Grid, IconButton, Snackbar, TextField, Typography } from '@mui/material';
+import { Alert, Box, Card, CardContent, CardMedia, CircularProgress, Grid, IconButton, Snackbar, Stack, TextField, Typography } from '@mui/material';
 import { useRouter } from 'next/router';
 import {  useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -30,7 +30,7 @@ const RaceTicketForm = ({raceData}) => {
     startTime,
     endTime,
   } = raceData;
-  console.log('wtfwtfw ==', raceData)
+  
   const handleCreateRaceTicket = async () => {
     let variables = {
       cost: amount,
@@ -48,7 +48,7 @@ const RaceTicketForm = ({raceData}) => {
   }; 
 
   return (
-    <>
+    <Stack sx={{margin: 5}}>
     <Snackbar open={showSuccess} autoHideDuration={2000} onClose={handleClose} anchorOrigin={{vertical: 'top', horizontal: 'center'}} key={'top'+'center'} >
       <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
         Success!
@@ -78,27 +78,32 @@ const RaceTicketForm = ({raceData}) => {
         </CardContent>
         <Box sx={{ width: '100%', display: 'flex', justifyContent: 'flex-end', pl: 1, pb: 1 }}>
           <AttachMoneyIcon color='action' sx={{color: 'black', fontSize: "40px", marginTop: 1}} />
-          <Typography sx={{color: 'black', fontSize: "40px", marginRight: 1}}>{ amount }</Typography>
+          <TextField
+            id="ticket-cost"
+            label="Cost"
+            type="number"
+            variant="standard"
+            multiline
+            sx={{
+              maxWidth: 80,
+              marginRight: -5,
+              
+            }}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            onChange={(e) => setAmount(e.target.value)}
+          />
+          {/* <Typography sx={{color: 'black', fontSize: "40px", marginRight: 1}}>{ amount }</Typography> */}
         </Box>
       </Box>
-      <Box display="flex" sx={{ '& > :not(style)': { m: 1 } }}>
+      <Box display="flex" sx={{ '& > :not(style)': { m: 1 }, maxHeight: 70 }}>
         <Fab onClick={handleCreateRaceTicket} size="medium" color='success'  aria-label="add">
           <AddIcon />
         </Fab>
       </Box>
     </Card>
-    <Grid container display="flex" direction="row" justifyContent="center" sx={{marginTop: 0}}>
-      <TextField
-        id="ticket-cost"
-        label="Enter Cost"
-        type="number"
-        InputLabelProps={{
-          shrink: true,
-        }}
-        onChange={(e) => setAmount(e.target.value)}
-      />
-    </Grid>
-    </>
+    </Stack>
   );
 }
 
