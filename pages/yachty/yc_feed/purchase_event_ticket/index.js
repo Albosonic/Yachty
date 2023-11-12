2a6b6f8b6913bfe5894ff29b777f44a126ce7239
albosonic <albertomadueno@gmail.com>
2023-11-12 15:10:11 -0800

    responsive fixes

--- pages/yachty/yc_feed/purchase_event_ticket/index.js
+++ pages/yachty/yc_feed/purchase_event_ticket/index.js
@@ -1,6 +1,8 @@
 import EventTicketForPurchase from "@/components/EventTicketForPurchase"
 import NavBar from "@/components/NavBar";
 import { EVENT_TICKET_FOR_PURCHASE } from "@/lib/gqlQueries/ycFeedgql";
+import { useQuery } from "@apollo/client";
+import { CircularProgress, Stack } from "@mui/material";
 import { useRouter } from "next/router";
 
 const YcEventTicketForPurchase = () => {
@@ -8,12 +10,16 @@ const YcEventTicketForPurchase = () => {
   const eventId = router.query.eventId;
   const {loading, data, error } = useQuery(EVENT_TICKET_FOR_PURCHASE, {variables:{eventId}, fetchPolicy: "no-cache" });
   console.log('data ===>', data);
-  if (loading) return <CircularProgress />
-  const eventData = yc_events[0];
+  console.log('eventId ===>', eventId);
+  console.log('loading ===>', loading);
+  if (loading || data === undefined) return <CircularProgress />
+  const eventData = data.yc_events[0];
   return (
     <>
       <NavBar />
-      <EventTicketForPurchase eventData={eventData} />
+      <Stack alignItems="center" >
+        <EventTicketForPurchase eventData={eventData} />
+      </Stack>
     </>
   )
 }
