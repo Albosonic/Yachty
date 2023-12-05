cbb6805244bd93d5f9287033d7a910564f2b85ef
albosonic <albertomadueno@gmail.com>
2023-12-04 18:20:06 -0800

    yachy new skin

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,11 +1,12 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useRouter } from 'next/router';
 import NavBar from '@/components/NavBar';
-import { Box, Button, CircularProgress, Container, Grid, Stack, Typography, useMediaQuery } from '@mui/material';
+import { Box, Button, CircularProgress, Container, Divider, Grid, Stack, Typography, useMediaQuery } from '@mui/material';
 import styles from '@/styles/Home.module.css'
 import { useUser } from '@auth0/nextjs-auth0/client';
 import AnchorIcon from '@mui/icons-material/Anchor';
 import { clearState } from '@/slices/actions/authActions';
+import LoadingYachty from '@/components/LoadingYachty';
 
 const Login = () => {
   const router = useRouter()
@@ -16,21 +17,76 @@ const Login = () => {
   // dispatch(clearState()) //for debugging purposes.
   const moreThan600px = useMediaQuery('(min-width:600px)');
 
-  if (isLoading) return <CircularProgress />
+  if (isLoading) return <LoadingYachty />
   if (user?.email_verified === true) router.push('/yachty', {query: { ycId }});
 
-  const waterLevel = moreThan600px ? "19%" : "29%";
+
 // https://yachty-letter-heads.s3.us-west-1.amazonaws.com/a2bb7f71-7b84-4db7-b9cb-306bf54a5af7
-  return (
-    <main style={{backgroundColor: '#052745', minHeight: '100vh'}}>
-      <Stack
-        spacing={6}
+
+  return (      
+    <Stack
+      spacing={2}
+      xs={3}
+      sx={{
+        height: '100vh',
+        backgroundColor: '#052745',
+        borderTop: '10px solid #461F00',
+        borderBottom: '10px solid #461F00',
+        overflow: "hidden",
+        overflowY: "scroll",
+        // border: '10px solid #461F00',
+      }}
+      alignItems="center"
+    >
+      <Typography
+        sx={{
+          color: '#FFFFFF',
+          fontFamily: 'Bradley Hand',
+          fontSize: 80,
+          margin: 2,  
+        }}>
+          Yachty
+        </Typography>
+      <Grid
+        container
+        spacing={2}
+        direction="column"
         alignItems="center"
+        justifyContent="center"
+        // flexGrow={1}
+        // sx={{ minHeight: '100vh' }}
       >
-        <AnchorIcon sx={{fontSize: 200}} />
-        <Button variant='standard' color="secondary" onClick={() => window.location = `${window.location.origin}/api/auth/login`}>Log in to Yachty</Button>
-      </Stack>
-    </main>
+        <Button
+          onClick={() => window.location = `${window.location.origin}/api/auth/login`}
+          variant='standard'
+          color="secondary"
+          sx={{
+            color: '#FFFFFF',
+            fontFamily: 'Bradley Hand',
+            fontSize: 40,
+          }}              
+        >
+            Log In
+        </Button>            
+        <AnchorIcon
+          sx={{
+            fontSize: 350,
+            color: '#c9c5c5',                
+          }}
+        />
+      </Grid>
+      <Button
+        onClick={() => window.location = `${window.location.origin}/api/auth/login`}
+        sx={{
+          color: '#FFFFFF',
+          fontFamily: 'Bradley Hand, cursive',
+          fontSize: 40,
+          position: 'absolute',              
+          bottom: 10          
+        }}>
+          Join The Club
+      </Button>
+    </Stack>      
   );
 }
 
