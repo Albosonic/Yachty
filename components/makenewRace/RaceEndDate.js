7e6f0915f837843bd9c5003d9336983e6d4c394d
albosonic <albertomadueno@gmail.com>
2024-01-10 13:05:23 -0800

    gate race date picks

--- components/makenewRace/RaceEndDate.js
+++ components/makenewRace/RaceEndDate.js
@@ -1,5 +1,5 @@
 import { RACE_FIELDS, clearNewRaceFieldsAct, makeNewRaceFieldAct } from "@/slices/actions/workingRaceActions";
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import { Button, Grid, Stack, TextField, Typography } from "@mui/material";
 import { DateTimeField, MobileDateTimePicker } from "@mui/x-date-pickers";
 import EastIcon from '@mui/icons-material/East';
 import Slide from '@mui/material/Slide';
@@ -8,32 +8,33 @@ import { useState } from "react";
 
 const SetRaceEnd = ({ callback }) => {
   const dispatch = useDispatch();
-  const [endDate, setEndDate] = useState(null);
-  const workingDate = useSelector(state => state.scheduler.workingRaceDate)  
+  const startDate = useSelector(state => state.workingRace.startDate);  
+  const [error, setError] = useState(false);
+  const [endDate, setEndDate] = useState(null);    
   const {IMAGE} = RACE_FIELDS;
   // dispatch(clearNewRaceFieldsAct()); for debugging...
   const editRaceEnd = () => {
+    const compareStart = new Date(startDate).getTime();
+    const compareEnd = new Date(endDate).getTime();
+    if (compareStart > compareEnd) return setError(true);
     let dateString = endDate.toString()
-    // let t = new Date(dateString)
-    // let date = getNormalCalanderDate(t.toISOString())
     dispatch(makeNewRaceFieldAct({endDate: dateString}));
     callback(IMAGE);
   }    
-
+  console.log('error ====', error)
   return (
     <>
       <Slide direction="right" in={true} mountOnEnter unmountOnExit>
         <Typography variant="h4">Race End</Typography>
       </Slide>
       <Grid sx={{minWidth: 300}} container justifyContent="space-between">
-        <MobileDateTimePicker          
-          onChange={(e) => setEndDate(e.$d)}        
-        />
+        <Stack>
+          <MobileDateTimePicker onChange={(e) => setEndDate(e.$d)} />
+          {error && <Typography color="error">cannot have end date before start date</Typography>}
+        </Stack>
           <Slide direction="left" in={true} mountOnEnter unmountOnExit>
             <Button onClick={editRaceEnd} endIcon={<EastIcon />}>next</Button>        
           </Slide>
-
-        
       </Grid>
     </>
   )
