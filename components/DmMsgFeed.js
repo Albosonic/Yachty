68d6dc4251ba70c0b131cde6ecb205062c8640d4
albosonic <albertomadueno@gmail.com>
2024-01-18 11:57:33 -0800

    dm loading and scroll

--- components/DmMsgFeed.js
+++ components/DmMsgFeed.js
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useRouter } from "next/router";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useMutation, useQuery } from "@apollo/client";
 import { INSERT_MESSAGE, POLL_ALL_MESSAGES } from "@/lib/gqlQueries/dmgql";
 import useMediaQuery from '@mui/material/useMediaQuery';
@@ -40,17 +40,24 @@ const getMsgFacade = (messages) => {
 const DmMsgFeed = () => {
   const router = useRouter();
   const currentRmId = router.query.rid;
-  const dispatch = useDispatch();    
+  const dispatch = useDispatch();
   const [inputMsg, setMessage] = useState('');
+  const [msgFacade, setMsgFacade] = useState([]);
   const memberId = useSelector(state => state.auth.member.id);
+  const profilePic = useSelector(state => state.auth.member.profilePic);
+  const anchor = useRef(null);
   const moreThan600px = useMediaQuery('(min-width:600px)');
   // const [showReactionOptions, setShowReactionOptions] = useState({ msgRef: null, showOptions: false });
   const {data: pollMsgData, loading: pollLoading, error: pollError} = useQuery(POLL_ALL_MESSAGES, {
     fetchPolicy: 'no-cache',
     variables: {roomId: currentRmId},
-    pollInterval: 5500,
+    pollInterval: 4000,
   });
   const [insertMessage, {loading: msgLoading}] = useMutation(INSERT_MESSAGE);
+
+  useEffect(() => {
+    setMsgFacade(getMsgFacade(pollMsgData?.messages))
+  }, [pollMsgData])
 
   if (pollLoading) return <LoadingYachty isRoot={false} />;
 
@@ -62,6 +69,7 @@ const DmMsgFeed = () => {
   const messageGridHeight = getMessageGridHeight();
 
   const sendMessage = async () => {
+    setMsgFacade([...msgFacade, {message: inputMsg, authorId: memberId, profilePic: profilePic, isSkeleton: true}])
     await insertMessage({
     variables: {
       roomId: currentRmId,
@@ -70,10 +78,14 @@ const DmMsgFeed = () => {
       createdAt: new Date().toISOString()
     }});
     dispatch(pollUserRooms());
-    setMessage('');    
+    setMessage('');
   }
 
-  const msgFacade = getMsgFacade(pollMsgData?.messages);
+  const scrollToEl = () => {
+    if (anchor.current) {
+      anchor.current.scrollIntoView({behavior: "smooth"})
+    }
+  }
 
   return (
     <Container sx={{margin: 0}} fixed maxWidth="sm">
@@ -89,7 +101,21 @@ const DmMsgFeed = () => {
           }}
         >
           {msgFacade.map(((msg, i) => {
-            const {message, authorId, profilePic} = msg;
+            const {message, authorId, profilePic, isSkeleton} = msg;
+            if (i === msgFacade.length -1) {
+              return (
+                <Grid
+                  key={msg.authorId + i + message}
+                  container
+                  margin={1}
+                  justifyContent="center"
+                  ref={anchor}
+                  onClick={scrollToEl()}
+                >
+                  <Msg msg={message} authorId={authorId} profilePic={profilePic} isSkeleton={isSkeleton} />
+                </Grid>
+              )
+            }
             return (
               <Grid
                 key={msg.authorId + i + message}
@@ -97,7 +123,7 @@ const DmMsgFeed = () => {
                 margin={1}
                 justifyContent="center"
               >
-                <Msg msg={message} authorId={authorId} profilePic={profilePic} />
+                <Msg msg={message} authorId={authorId} profilePic={profilePic} isSkeleton={isSkeleton} />
               </Grid>
             )
           }))}
--- components/Message.js
+++ components/Message.js
@@ -1,20 +1,20 @@
-import { Avatar, Grid, Stack, Typography } from "@mui/material";
+import { Avatar, Grid, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 
-const Msg = ({ msg, authorId, profilePic }) => {
+const Msg = ({ msg, authorId, profilePic, isSkeleton }) => {
   const memberId = useSelector(state => state.auth.member.id);
-    const leftOrRight = authorId === memberId ? "flex-start" : "flex-end";
-    return (
-      <Grid container justifyContent={leftOrRight} sx={{ width: '80%'}} >
-        {authorId === memberId ? (
-          <>
-          <Avatar src={profilePic || userPic}
+  const leftOrRight = authorId === memberId ? "flex-start" : "flex-end";
+  return (
+    <Grid container justifyContent={leftOrRight} sx={{ width: '80%'}} >
+      {authorId === memberId ? (
+        <>
+          <Avatar src={profilePic}
             sx={{
               width: 20,
               height: 20,
               marginRight: -.3,
             }}
-          />          
+          />
           <Typography
             sx={{
               border: '1px solid grey',
@@ -23,38 +23,38 @@ const Msg = ({ msg, authorId, profilePic }) => {
               paddingBottom: .4,
               paddingLeft: 1.5,
               paddingRight: 1.5,
-              opacity: .5,              
+              opacity: isSkeleton ? .1 : .5,
             }}
           >
             {msg}
-          </Typography>          
+          </Typography>
         </>
-        ) : (
-          <>
-            <Typography
-              sx={{
-                border: '1px solid grey',
-                borderRadius: 3.5,
-                paddingTop: .4,
-                paddingBottom: .4,
-                paddingLeft: 1.5,
-                paddingRight: 1.5,
-                opacity: .5,
-              }}
-            >
-              {msg}
-            </Typography>
-            <Avatar src={profilePic || userPic}
-              sx={{
-                width: 20,
-                height: 20,
-                marginRight: -.3,
-              }}
-            />
-          </>
-        )}
-      </Grid>
-    )
-  }
+      ) : (
+        <>
+          <Typography
+            sx={{
+              border: '1px solid grey',
+              borderRadius: 3.5,
+              paddingTop: .4,
+              paddingBottom: .4,
+              paddingLeft: 1.5,
+              paddingRight: 1.5,
+              opacity: .5,
+            }}
+          >
+            {msg}
+          </Typography>
+          <Avatar src={profilePic}
+            sx={{
+              width: 20,
+              height: 20,
+              marginRight: -.3,
+            }}
+          />
+        </>
+      )}
+    </Grid>
+  )
+}
 
-  export default Msg;
+export default Msg;
