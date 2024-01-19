7dd0c2fd67a0c01865020b83396a66c0dd5367b4
albosonic <albertomadueno@gmail.com>
2024-01-19 15:33:51 -0800

    clean up

--- components/NewUserDialog.js
+++ components/NewUserDialog.js
@@ -1,5 +1,5 @@
 import { forwardRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import Button from '@mui/material/Button';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
@@ -7,7 +7,6 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
-import { useRouter } from 'next/router';
 import EditProfilePic from './EditProfilePic';
 import { Stack } from '@mui/material';
 import UpdateName from './UpdateName';
