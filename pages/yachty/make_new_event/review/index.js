d95f496b9ee95171c7f94d9cf5de888b8fe181f1
albosonic <albertomadueno@gmail.com>
2024-01-26 14:20:07 -0800

    minor bugs

--- pages/yachty/make_new_event/review/index.js
+++ pages/yachty/make_new_event/review/index.js
@@ -1,11 +1,20 @@
 import NavBar from "@/components/NavBar";
 import EventReviewPoster from "@/components/makenewEvent/EventReviewPoster";
+import { Grid } from "@mui/material";
 
 const EventPreviewReview = () => {
   return (
     <>
       <NavBar />
-      <EventReviewPoster />
+      <Grid        
+        container
+        direction="column"
+        alignItems="center"
+        width="100%"          
+        padding={5}      
+      >
+        <EventReviewPoster />
+      </Grid>
     </>
   )
 }
